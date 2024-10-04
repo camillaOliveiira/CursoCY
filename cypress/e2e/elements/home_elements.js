0ab72018ad1ac6677c195383560335f495e57ce3
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-10-04 20:12:05 -0300

    home_Elements

---
+++ cypress/e2e/elements/home_elements.js
@@ -0,0 +1,7 @@
+class HomeElements {
+
+    btnHideCookie = () => { return '#cookie_stop' }
+    selectAccount = () => { return 'Account' }
+    btnLogin = () => { return 'Login' }
+
+} export default HomeElements
