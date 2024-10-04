96bf60f39df24cee575d2548e9812816eb4724de
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-10-04 20:03:25 -0300

    delete

--- cypress/e2e/teste.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Login', function(){
-    it('Sign in', function(){
-        cy.visit('www.site.com.br')
-        cy.get('input[type="email"]').type('endereco@email.com')
-        cy.get('input[type="password"]').type('senha')
-        cy.get('btn').contains('Entrar').click()
-    })
-});
