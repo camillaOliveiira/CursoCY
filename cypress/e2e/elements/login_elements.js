b0eed70ca57e254a61873008dc36f3dccb8f1d3a
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-10-04 20:19:46 -0300

    login_Elements

---
+++ cypress/e2e/elements/login_elements.js
@@ -0,0 +1,8 @@
+class LoginElements {
+//quando tiver id usar # na frente do nome 
+//quando não tiver id usar texto 
+    inputEmail = () => { return '#email' }
+    inputPassword = () => { return '#password' }
+    btnLogin = () => { return '#submitBTN' }
+
+} export default LoginElements
