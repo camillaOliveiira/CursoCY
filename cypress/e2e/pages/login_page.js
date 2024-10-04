9e1a785b053f002b723e927450e1cdbc2f40646c
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-10-04 20:56:22 -0300

    login_page

--- cypress/e2e/pages/login_page.js
+++ cypress/e2e/pages/login_page.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+/*/// <reference types="Cypress" />
 
 import LoginElements from "../elements/login";
 //instancia
@@ -19,4 +19,39 @@ class LoginPage {
     clickEmLogin() {
         cy.get(loginElements.btnEnter()).click()
     }
-} export default LoginPage()
+} export default LoginPage()*/
+/// <reference types="Cypress" />
+import HomeElements from "../elements/home_elements";
+import LoginElements from "../elements/login_elements";
+
+const loginElements = new LoginElements
+const homeElements = new HomeElements
+
+const url = Cypress.config('baseUrl')
+
+class LoginPage {
+
+    accessHomePage() {
+        //abre a pagina
+        cy.visit(url);
+        cy.wait(3000);
+        //clicar em hide no popup de cookies
+        cy.get(homeElements.btnHideCookie()).click();
+    }
+
+    accessLoginPage() {
+        //quando se trata de texto usamos o cy.contains = contem
+        cy.contains(homeElements.selectAccount()).click();
+        cy.contains(homeElements.btnLogin()).click();
+    }
+
+    fillLoginInfo(email, password) {
+        cy.get(loginElements.inputEmail()).type(email);
+        cy.get(loginElements.inputPassword()).type(password);
+    }
+
+    submitLogin() {
+        cy.get(loginElements.submitLogin()).click();
+    }
+
+} export default LoginPage
