5e465217687b11eef0e16d71bad85efc4e577a4d
Camila Oliveira <camila_oliveiira1@hotmail.com>
2024-10-08 15:12:12 -0300

    Aula 31 - mapeando elementos de registro de usuario

--- cypress/e2e/elements/register_elements.js
+++ cypress/e2e/elements/register_elements.js
@@ -1,3 +1,20 @@
 export default class RegisterElements {
-    
+
+    inputFirstName = () => { return '[@placeholder="First Name"]' }
+    inputLastName = () => { return '[@placeholder="Last Name"]' }
+    inputAddress = () => { return '//*[@id="basicBootstrapForm"]/div[2]/div/textarea' }
+    inputAddressEmail = () => { return '#eid' }
+    inputPhoneNumber = () => { return '//*[@id="basicBootstrapForm"]/div[4]/div/input' }
+    selectFile = () => { return '#imagescr' }
+    checkRadioGender = () => { return '[@type="radio"]' }
+    checkHobbies = () => { return '[@type="checkbox"]' }
+    inputLanguages = () => { return '[#msdd]' }
+    selectCountry = () => { return '//*[@id="basicBootstrapForm"]/div[10]/div/span/span[1]/span' }
+    selectYear = () => { return '#yearbox' }
+    selectMonth = () => { return '[@placeholder="Month"]' }
+    selectDay = () => { return 'daybox' }
+    inputPass = () => { return '#firstpassword' }
+    inputConfirmPass = () => { return '#secondpassword' }
+    submitRegister = () => { return '#submitbtn' }
+    refreshRegister = () => {return '#Button1'}
 }
